83c71b99c7566396f716d2b750b6f7d7ca7dbe11
Daikeri <daikerikot@yandex.ru>
2023-02-12 21:08:36 +0300

    :bee: :bee: :bee:

---
+++ LW_1/task_3.2.rb
@@ -0,0 +1,37 @@
+if (Gem.win_platform?)
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+def min(source_arr)
+  m = source_arr[0]
+  source_arr.each { |i| m = i if i < m }
+  m
+end
+
+def first_pos(source_arr)
+  n = source_arr.size
+  for ind in 0..n do
+    return ind if source_arr[ind] > 0
+  end
+end
+
+puts 'Укажите номер необходимой функции, а также путь к файлу с массивом'
+func, path = gets.chomp.split(' ')
+
+file_info = File.new path
+arr = file_info.gets.chomp.split(' ')
+arr = arr.map { |el| el.to_i }
+
+
+case  func
+when '1'
+  puts "Минимальный элемент:#{min arr}"
+when '2'
+  puts "Индекс первого положительного элемента:#{first_pos arr}"
+  else puts 'Указанная функция не найдена'
+end
