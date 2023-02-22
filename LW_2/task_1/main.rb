2ab5129274ce2551df9f454f9d39c1056b53547d
Daikeri <daikerikot@yandex.ru>
2023-02-23 01:49:47 +0300

    Point 5

--- LW_2/task_1/main.rb
+++ LW_2/task_1/main.rb
@@ -1,7 +1,12 @@
 require_relative 'student.rb'
 
-test1 = Student.new('Алешина', 'Василиса', 'Денисовна', '+7 924 209 16 11')
-test2 = Student.new('Крылов', 'Григорий', 'Евгеньевич', '+7 978 111 90 37')
+test1 = Student.new('Алешина', 'Василиса', 'Денисовна',
+                    _phone: '+7 924 209 16 11', _id: 101)
+test2 = Student.new('Крылов', 'Григорий', 'Евгеньевич',
+                    _telegram: '@picklePop', _id: 43, _email: 'krilov@yandex.ru')
+
+test3 = Student.new('Богомолов', 'Александр', 'Романович')
 
 test1.get_info
 test2.get_info
+test3.get_info
--- LW_2/task_1/student.rb
+++ LW_2/task_1/student.rb
@@ -2,17 +2,26 @@
 class Student
 
   attr_accessor :last_name, :first_name,
-                :patronymic, :phone
+                :patronymic, :id,
+                :phone, :telegram,
+                :email, :git
 
-  def initialize(_last_name, _first_name, _patronymic, _phone)
+  def initialize(_last_name, _first_name, _patronymic, _id:'unknown', _phone:'unknown',
+                 _telegram:'unknown', _email:'unknown', _git:'unknown')
+
     @last_name = _last_name
     @first_name = _first_name
     @patronymic = _patronymic
+    @id = _id
     @phone = _phone
+    @telegram = _telegram
+    @email = _email
+    @git = _git
+
   end
 
   def get_info
-    puts "Last Name - #{self.last_name} First Name - #{self.first_name} Patronymic - #{self.patronymic} Phone - #{self.phone}"
+    puts "\nLast Name - #{self.last_name}\nFirst Name - #{self.first_name}\nPatronymic - #{self.patronymic}\nID - #{self.id}\nPhone - #{self.phone}\nTelegram - #{self.telegram}\nEmail - #{self.email}\nGit - #{self.git}\n"
   end
 
 end
