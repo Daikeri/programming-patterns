f53b93422638f87402d1da13f7746a568f68b40a
Daikeri <daikerikot@yandex.ru>
2023-02-25 04:18:00 +0300

    Point 8 - 10

--- LW_2/task_1/main.rb
+++ LW_2/task_1/main.rb
@@ -1,17 +1,9 @@
 require_relative 'student.rb'
 
-test1 = Student.new({last_name:'Алешина', first_name:'Василиса', patronymic:'Денисовна',
-                     phone:'+7 924 209 16 11'})
-test1.get_info
+test1 = Student.new({last_name:'Алешина', first_name:'Василиса', patronymic:'Денисовна', phone:'+7 924 209 16 11',
+                     git:'https://github.com/Daikeri'})
 
 test2 = Student.new({last_name:'Крылов', first_name:'Григорий', patronymic:'Евгеньевич',
-                     telegram:'@picklePop', id:43, email: 'krilov@yandex.ru'})
-test2.get_info
+                     telegram:'@kitmamymav', id:43, email: 'krilov@yandex.ru'})
 
-test3 = Student.new({last_name:'Богомолов', first_name:'Александр', patronymic:'Романович', phone: '+384 136 809'})
-
-test3.get_info
-
-
-
-
+test3 = Student.new({last_name:'Богомолов', first_name:'Александр', patronymic:'Романович'})
--- LW_2/task_1/student.rb
+++ LW_2/task_1/student.rb
@@ -2,12 +2,24 @@
 class Student
 
   PHONE = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/
-  TELEGRAM = ''
-  EMAIL = ''
-  GIT = ''
+  TELEGRAM = /^@[A-Za-z\d_]{5,32}$/
+  EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  GIT = /\Ahttps:\/\/github\.com\/\w+\z/
 
   def Student.correct_phone?(str)
     str.match?(PHONE)
+  end
+
+  def Student.correct_telegram?(str)
+    str.match?(TELEGRAM)
+  end
+
+  def Student.correct_email?(str)
+    str.match?(EMAIL)
+  end
+
+  def Student.correct_git?(str)
+    str.match?(GIT)
   end
 
   attr_accessor :last_name, :first_name,
@@ -20,21 +32,44 @@ class Student
     @first_name = arg[:first_name]
     @patronymic = arg[:patronymic]
     @id = arg[:id]
-    @phone = arg[:phone]
-    @telegram = arg[:telegram]
-    @email = arg[:email]
-    @git = arg[:git]
 
     raise(ArgumentError, 'ФИО - обязательные параметры!') unless @last_name && @first_name && @patronymic
 
-    if @phone
-      raise(ArgumentError, 'Неккоректный формат номера телефона!') unless Student.correct_phone?(@phone)
-    end
+    set_contacts(arg[:phone], arg[:telegram], arg[:email], arg[:git])
+
+    validate
 
   end
 
   def get_info
-    puts "\nLast Name - #{self.last_name}\nFirst Name - #{self.first_name}\nPatronymic - #{self.patronymic}\nID - #{self.id}\nPhone - #{self.phone}\nTelegram - #{self.telegram}\nEmail - #{self.email}\nGit - #{self.git}\n"
+    puts "\nLast Name - #{last_name}\nFirst Name - #{first_name}\nPatronymic - #{patronymic}\nID - #{id}\nPhone - #{phone}\nTelegram - #{telegram}\nEmail - #{email}\nGit - #{git}\n"
+  end
+
+  protected
+
+  def set_contacts(phone, telegram, email, git)
+    raise(ArgumentError, 'Неверный формат номера телефона!') if phone && !Student.correct_phone?(phone)
+    raise(ArgumentError, 'Неверный формат имени пользователя телеграмм!') if telegram && !Student.correct_telegram?(telegram)
+    raise(ArgumentError, 'Неверный формат электронной почты!') if email && !Student.correct_email?(email)
+    raise(ArgumentError, 'Неверный формат ссылки на Гит!') if git && !Student.correct_git?(git)
+
+    @phone = phone
+    @telegram = telegram
+    @email = email
+    @git = git
+  end
+
+  def validate
+    valid_git
+    valid_other
+  end
+
+  def valid_git
+    raise(ArgumentError, 'Обязательно должен быть указан Гит!') unless git
+  end
+
+  def valid_other
+     raise(ArgumentError, 'Должен быть указан хотя бы один контакт!') unless phone || telegram || email
   end
 
 end
