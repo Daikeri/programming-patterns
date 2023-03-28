a2156630ac1e8d6f76014b735d5b2a1f018633c6
daikeri <daikerikot@yandex.ru>
2023-03-28 05:28:43 +0300

    :angry: :angry: :angry:

---
+++ LW_2/task_4_strategy/file_proc_strategy.rb
@@ -0,0 +1,11 @@
+
+
+module FileProcStrategy
+  def read_the_file(path)
+    raise "The 'read_the_file' method is not implemented!"
+  end
+
+  def write_to_file(path)
+    raise "The 'write_to_file' method is not implemented!"
+  end
+end
